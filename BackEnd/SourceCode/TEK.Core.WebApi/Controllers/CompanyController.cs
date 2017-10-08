ea41b357a090e0189ff84c7084b6e8989c513f5d
Sergey V. Kovalenko <k_smithus@yahoo.com>
2017-10-08 12:00:49 -0500

    'Code'

--- BackEnd/SourceCode/TEK.Core.WebApi/Controllers/CompanyController.cs
+++ BackEnd/SourceCode/TEK.Core.WebApi/Controllers/CompanyController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-using TEK.Core.Entity;
 using TEK.Core.ResourceAccess.EF;
 
 namespace TEK.Core.WebApi.Controllers
@@ -25,46 +26,19 @@ namespace TEK.Core.WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-			Console.Write("\nGot request...");
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.Write($"\n--- Got request [{threadId}]...");
+            var watch = Stopwatch.StartNew();
 
-			var result = await this.dataContext.Set<Company>()
-                .OrderBy(x => x.Name).
-				Take(80000).
-				ToListAsync();
+            var result = await this.dataContext.Companies
+                .OrderBy(x => x.Name)
+				.Take(1000000)
+				.ToListAsync();
 
-			Console.WriteLine("Load from database...");
-
-            return Ok(result);
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            var result = await this.dataContext.Set<Company>()
-                .SingleOrDefaultAsync(x => x.Id == id);
-
-            if (result == null) return NotFound($"Id {id} not found");
+            watch.Stop();
+			Console.WriteLine($"[{threadId}] Load from database...({watch.ElapsedMilliseconds} miliSec)");
 
             return Ok(result);
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
---
+++ BackEnd/SourceCode/TEK.Core.WebApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEK.Core.WebApi.Controllers
+{
+    [Route("api/Test")]
+    [Produces("application/json")]    
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            Console.Clear();
+            return string.Empty;
+        }
+    }
+}
