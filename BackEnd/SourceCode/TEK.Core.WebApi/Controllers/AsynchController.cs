4ea3829dc24960f59f07f5b0efc76089967f3c84
Sergey V. Kovalenko <k_smithus@yahoo.com>
2017-10-08 22:39:29 -0500

    'Code'

---
+++ BackEnd/SourceCode/TEK.Core.WebApi/Controllers/AsynchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+
+using TEK.Core.ResourceAccess.EF;
+
+namespace TEK.Core.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [EnableCors("AllowSpecificOrigin")]
+	public class AsynchController : Controller
+    {
+        private readonly DataContext dataContext;
+
+        public AsynchController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [Route("api/[controller]/{count:int}")]
+        public async Task<IActionResult> Get(int count)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.Write($"\n---> Got request. Count: ({count}) Thread: [{threadId}]...");
+            var watch = Stopwatch.StartNew();
+
+            var result = await this.dataContext.Companies
+                .OrderBy(x => x.Name)
+				.Take(count)
+				.ToListAsync();
+
+            watch.Stop();
+			Console.WriteLine($"---> [{threadId}] Load aSynch...({watch.ElapsedMilliseconds} miliSec)");
+
+            return Ok(result);
+        }
+    }
+}
---
+++ BackEnd/SourceCode/TEK.Core.WebApi/Controllers/SynchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+using TEK.Core.ResourceAccess.EF;
+
+namespace TEK.Core.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [EnableCors("AllowSpecificOrigin")]
+    public class SynchController : Controller
+    {
+        private readonly DataContext dataContext;
+
+        public SynchController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [Route("api/[controller]/{count:int}")]
+        public IActionResult Get(int count)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.Write($"\n---> Got request. Count: ({count}) Thread: [{threadId}]...");
+            var watch = Stopwatch.StartNew();
+
+            var result = this.dataContext.Companies
+                .OrderBy(x => x.Name)
+                .Take(count)
+                .ToList();
+
+            watch.Stop();
+            Console.WriteLine($"---> [{threadId}] Load synch...({watch.ElapsedMilliseconds} miliSec)");
+
+            return Ok(result);
+        }
+    }
+}
