438c0785701d6242d02e893ca60938b2fc14e537
Sergey V. Kovalenko <k_smithus@yahoo.com>
2017-11-02 16:05:35 -0400

    code cleanup

---
+++ BackEnd/SourceCode/TEK.Core.WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+using TEK.Core.Entity;
+
+namespace TEK.Core.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Company")]
+    public class CompanyController : Controller
+    {
+        // GET: api/Company
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Company/5
+        [HttpGet("{id}", Name = "Get")]
+        public Company Get(int id)
+        {
+            return new Company { Id = 123, Name = "Big Company" };
+        }
+    }
+}
