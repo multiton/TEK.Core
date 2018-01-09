47536b92842d6dd4d0658a84071a9aed3d23dcb9
multiton <k_smithus@yahoo.com>
2018-01-09 10:52:38 -0500

    Code cleanup

--- FrontEnd/angular/src/app/core/companies/company/company.component.ts
+++ FrontEnd/angular/src/app/core/companies/company/company.component.ts
@@ -5,22 +5,22 @@ import { Company } from '../shared/company.model';
 import { CompanyService } from '../shared/company.srevice';
 
 @Component({
-  selector: 'app-company-list',
-  templateUrl: './company-list.component.html',
-  styleUrls: ['./company-list.component.css']
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
 })
-export class CompanyListComponent implements OnInit, OnDestroy
+export class CompanyComponent implements OnInit, OnDestroy
 {
-  private companies : Company[];
+  private company : Company;
   private subscription: ISubscription;
 
   constructor(private companyService : CompanyService) { }
 
   ngOnInit(): void
   {
-    this.subscription = this.companyService
-      .getAll()
-      .subscribe(data => this.companies = data);
+    // this.subscription = this.companyService
+    //   .get(123)
+    //   .subscribe(data => this.company = data);
   }
 
   ngOnDestroy()
