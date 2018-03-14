44075bd9395b4ae6d72f371fdb01435cfa38adb4
multiton <k_smithus@yahoo.com>
2018-03-13 20:06:41 -0400

    Code cleanup

--- FrontEnd/angular/src/app/rest-service-base.ts
+++ FrontEnd/angular/src/app/rest-service-base.ts
@@ -7,41 +7,36 @@ import 'rxjs/add/operator/catch';
 
 export abstract class RestServiceBase<T>
 {   
-    constructor(protected http: HttpClient, protected actionUrl: string) { }
+    private baseUrl : string;
+
+    constructor(protected http: HttpClient, protected actionUrl: string)
+    {
+        this.baseUrl = `${environment.apiEndpoint}${this.actionUrl}`;
+    }
   
     getAll() : Observable<T[]>
     {
-        return this.http
-            .get<T[]>(`${environment.apiEndpoint}${this.actionUrl}`)
-            .catch(this.handleError);
+        return this.http.get<T[]>(this.baseUrl).catch(this.handleError);
     }
 
     getSingle(id: number) : Observable<T>
     {
-        return this.http
-            .get<T>(`${environment.apiEndpoint}${this.actionUrl}/${id}`)
-            .catch(this.handleError);
+        return this.http.get<T>(`${this.baseUrl}/${id}`).catch(this.handleError);
     }
 
-    post<T>(item: T)
+    post<T>(item: T) : Observable<T>
     {
-        return this.http
-            .post<T>(`${environment.apiEndpoint}${this.actionUrl}`, item)
-            .catch(this.handleError);
+        return this.http.post<T>(this.baseUrl, item).catch(this.handleError);
     }
     
-    put<T>(id: number, item: T)
+    put<T>(id: number, item: T) : Observable<T>
     {
-        return this.http
-            .put<T>(`${environment.apiEndpoint}${this.actionUrl}/${id}`,item)
-            .catch(this.handleError);;
+        return this.http.put<T>(`${this.baseUrl}/${id}`, item).catch(this.handleError);
     }
     
     delete(id: number) : Observable<T>
     {
-        return this.http
-            .delete(`${environment.apiEndpoint}${this.actionUrl}/${id}`)
-            .catch(this.handleError);
+        return this.http.delete(`${this.baseUrl}/${id}`).catch(this.handleError);
     }
 
     private handleError(err: HttpErrorResponse)
